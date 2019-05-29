352c3eb1b59641e04411d6cef7235f794a383322
mahuajian <mahuajian@gmail.com>
2019-05-29 16:58:05 +0800

    init test pod

---
+++ MyTest/Classes/ACTest.h
@@ -0,0 +1,16 @@
+//
+//  ACTest.h
+//  FBSnapshotTestCase
+//
+//  Created by ac on 2019/5/29.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ACTest : NSObject
++(void)log:(NSString *)logStr; //as
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MyTest/Classes/ACTest.m
@@ -0,0 +1,15 @@
+//
+//  ACTest.m
+//  FBSnapshotTestCase
+//
+//  Created by ac on 2019/5/29.
+//
+
+#import "ACTest.h"
+
+@implementation ACTest
++(void)log:(NSString *)logStr
+{
+    NSLog(@"ac log - %@",logStr);
+}
+@end
